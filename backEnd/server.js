17bda2d4427d4a09499bf00f441a3d2b5857949a
Juraj Piovar <jpiovar@sk.ibm.com>
2022-07-29 13:42:45 +0200

    correction

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,23 +1,20 @@
-const express = require("express");
+import express from 'express';
 const port = 3000;
 const app = express();
-​
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+app.get('/janko', (req, res) => {
+  res.send('Hello janko!');
 });
-​
-app.get("/janko", (req, res) => {
-  res.send("Hello janko!");
-});
-​
-app.get("/json", (req, response) => {
-  response.writeHead(200, { "Content-Type": "application/json" });
-  var json = JSON.stringify({
-    another: "item",
+app.get('/json', (req, response) => {
+  response.writeHead(200, { 'Content-Type': 'application/json' });
+  const json = JSON.stringify({
+    another: 'item',
   });
   response.end(json);
 });
-​
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
